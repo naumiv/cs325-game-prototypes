33e13036ae63e85fa9a0b4e562db133f9547ca43
Naumi <naumi.varma1@gmail.com>
2021-02-10 13:15:16 -0500

    Update main.js

--- digital-assignment-1/js/main.js
+++ digital-assignment-1/js/main.js
@@ -7,12 +7,12 @@ class MyScene extends Phaser.Scene {
     }
     
     preload() {
-        this.load.image('farm', '/assets/farm.png');
+        this.load.image('farm', 'assets/farm.png');
         this.load.spritesheet('chicken', 'assets/chicken.png', { frameWidth: 52, frameHeight: 60 });
-        this.load.image('ground', '/assets/platform.png');
-        this.load.image('star', '/assets/star.png');
-        this.load.image('bomb', '/assets/bomb.png');
-        this.load.spritesheet('dude', '/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
     }
     
     create() {
@@ -24,27 +24,27 @@ class MyScene extends Phaser.Scene {
         platforms.create(50, 250, 'ground');
         platforms.create(750, 220, 'ground');
 
-        player = this.physics.add.sprite(100, 450, 'dude');
+        player = this.physics.add.sprite(100, 450, 'chicken');
 
         player.setBounce(0.2);
         player.setCollideWorldBounds(true);
 
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frames: this.anims.generateFrameNumbers('chicken', { start: 0, end: 3 }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
             key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
+            frames: [ { key: 'chicken', frame: 4 } ],
             frameRate: 20
         });
 
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frames: this.anims.generateFrameNumbers('chicken', { start: 5, end: 8 }),
             frameRate: 10,
             repeat: -1
         });
