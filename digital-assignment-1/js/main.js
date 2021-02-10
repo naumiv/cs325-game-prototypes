2d3ea2338e84c89f7c776cc7c549558cb3c79e44
Naumi <naumi.varma1@gmail.com>
2021-02-10 12:24:49 -0500

    Update main.js

--- digital-assignment-1/js/main.js
+++ digital-assignment-1/js/main.js
@@ -1,56 +1,58 @@
 import "./phaser.js";
 
-// You can copy-and-paste the code from any of the examples at https://examples.phaser.io here.
-// You will need to change the `parent` parameter passed to `new Phaser.Game()` from
-// `phaser-example` to `game`, which is the id of the HTML element where we
-// want the game to go.
-// The assets (and code) can be found at: https://github.com/photonstorm/phaser3-examples
-// You will need to change the paths you pass to `this.load.image()` or any other
-// loading functions to reflect where you are putting the assets.
-// All loading functions will typically all be found inside `preload()`.
-
-// The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
-
 class MyScene extends Phaser.Scene {
     
-    constructor() {
+   constructor() {
         super();
-        
-        this.bouncy = null;
     }
     
     preload() {
-        // Load an image and call it 'logo'.
-        this.load.image( 'logo', 'assets/jelly.png' );
+        this.load.image('farm', '/assets/farm.png');
+        this.load.spritesheet('chicken', 'assets/chicken.png', { frameWidth: 52, frameHeight: 60 });
+        this.load.image('ground', '/assets/platform.png');
+        this.load.image('star', '/assets/star.png');
+        this.load.image('bomb', '/assets/bomb.png');
+        this.load.spritesheet('dude', '/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
     }
     
     create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        this.bouncy = this.physics.add.sprite( this.cameras.main.centerX, this.cameras.main.centerX, 'logo' );
-        
-        // Make it bounce off of the world bounds.
-        this.bouncy.body.collideWorldBounds = true;
-        
-        // Make the camera shake when clicking/tapping on it.
-        this.bouncy.setInteractive();
-        this.bouncy.on( 'pointerdown', function( pointer ) {
-            this.scene.cameras.main.shake(500);
-            });
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        let style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        let text = this.add.text( this.cameras.main.centerX, 15, "Hello, Hemangini Varma", style );
-        text.setOrigin( 0.5, 0.0 );
+        this.add.image(400, 300, 'farm');
+
+        platforms = this.physics.add.staticGroup();
+        platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        platforms.create(600, 400, 'ground');
+        platforms.create(50, 250, 'ground');
+        platforms.create(750, 220, 'ground');
+
+        player = this.physics.add.sprite(100, 450, 'dude');
+
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.physics.add.collider(player, platforms);
     }
     
     update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        this.bouncy.rotation = this.physics.accelerateToObject( this.bouncy, this.input.activePointer, 500, 500, 500 );
     }
 }
 
@@ -59,6 +61,15 @@ const game = new Phaser.Game({
     parent: 'game',
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: MyScene,
-    physics: { default: 'arcade' },
-    });
+});
+
+var platforms;
+var player;
