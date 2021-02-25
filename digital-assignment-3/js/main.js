02e442275de3a037fd925d45f55ecfd55947ce7b
Naumi <naumi.varma1@gmail.com>
2021-02-25 04:47:17 -0500

    Update main.js

--- digital-assignment-3/js/main.js
+++ digital-assignment-3/js/main.js
@@ -1,56 +1,159 @@
 import "./phaser.js";
 
-// You can copy-and-paste the code from any of the examples at https://examples.phaser.io here.
-// You will need to change the `parent` parameter passed to `new Phaser.Game()` from
-// `phaser-example` to `game`, which is the id of the HTML element where we
-// want the game to go.
-// The assets (and code) can be found at: https://github.com/photonstorm/phaser3-examples
-// You will need to change the paths you pass to `this.load.image()` or any other
-// loading functions to reflect where you are putting the assets.
-// All loading functions will typically all be found inside `preload()`.
-
-// The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
-
 class MyScene extends Phaser.Scene {
     
     constructor() {
         super();
-        
-        this.bouncy = null;
     }
     
     preload() {
-        // Load an image and call it 'logo'.
-        this.load.image( 'logo', 'assets/phaser.png' );
+        this.load.image( 'morning', 'assets/morning.png' );
+        this.load.image( 'noon', 'assets/noon.png' );
+        this.load.image( 'afternoon', 'assets/afternoon.png' );
+        this.load.image( 'sunset', 'assets/sunset.png' );
+        this.load.image( 'evening', 'assets/evening.png' );
+        this.load.image( 'pal', 'assets/pal.png' );
+        this.load.image( 'dino', 'assets/dino1.png' );
+        this.load.image( 'textbox', 'assets/textbox.png' );
+        this.load.audio('adventure', 'assets/adventure.mp3');
+        this.load.audio('excavate', 'assets/short_dig.mp3');
     }
     
     create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        this.bouncy = this.physics.add.sprite( this.cameras.main.centerX, this.cameras.main.centerX, 'logo' );
-        
-        // Make it bounce off of the world bounds.
-        this.bouncy.body.collideWorldBounds = true;
-        
-        // Make the camera shake when clicking/tapping on it.
-        this.bouncy.setInteractive();
-        this.bouncy.on( 'pointerdown', function( pointer ) {
-            this.scene.cameras.main.shake(500);
-            });
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        let style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        let text = this.add.text( this.cameras.main.centerX, 15, "Build something amazing.", style );
-        text.setOrigin( 0.5, 0.0 );
+        this.anims.create({
+            key: 'day',
+            frames: [
+                { key: 'morning', duration: 20000 },
+                { key: 'noon', duration: 21500 },
+                { key: 'afternoon', duration: 21500 },
+                { key: 'sunset', duration: 21500 },
+                { key: 'evening', duration: 21500 }
+            ],
+            timeScale: 1,
+            showOnStart: true,
+        });
+
+        this.add.sprite(400, 250, 'morning').play('day');
+
+        const textbox = this.add.image(600, 350, 'textbox');
+        const dino = this.add.image(50, 50, 'dino');
+        var gameover = this.add.text(225, 150, '', {fontFamily: 'Georgia', fontSize: '40px', fill: '#fff', align: 'center' });
+        const info = this.add.text(100, 17, '', {fontFamily: 'Georgia', fontSize: '20px', fill: '#fff' });
+        var dig = this.add.text(515, 330, '', {fontFamily: 'Georgia', fontSize: '20px', fill: '#000', align: 'center' });
+        dig.setWordWrapWidth(200, true);
+
+        var bg_music = this.sound.add('adventure').setLoop(true).setVolume(0.1);
+        bg_music.play();
+        var excavate = this.sound.add('excavate');
+
+
+        var posEvents = ["You found a T-Rex tooth!", "You found a Diplodocus femur!", "You found a Stegosaurus spike!",
+                         "You found a Pterodactyl wing!", "You found a Brontosaurus spine!", "You found a Trilobite shell",
+                         "You found a Plesiosaur fin bone!", "You found a Velociraptor claw!", "You found a Brachiosaurus leg bone!"];
+
+        // var negEvents = ["Oh no you lost a grant!", "just dirt ... and more dirt", "Yikes! You got stung by a scorpion!",
+        //                  "just dirt ... and more dirt", "Uh oh the excavation collapsed!", "just dirt ... and more dirt"];
+        var negEvents = ["Oh no you lost a grant!", "just dirt ... and more dirt", "just dirt ... and more dirt",
+                         "just dirt ... and more dirt","Yikes! You got stung by a scorpion!","just dirt ... and more dirt",
+                         "just dirt ... and more dirt", "just dirt ... and more dirt", "Uh oh the excavation collapsed!"];
+
+        var startMon = Math.floor((Math.random() * 20000) + 1);
+
+        dino.setDataEnabled();
+        dino.setData('money', startMon);
+        dino.setData('fossils', 0);
+        dino.setData('rank', 'dinosaur enthusiast');
+
+        textbox.setDataEnabled();
+        textbox.setData('event', 'nothing so far');
+
+        info.setText([
+            'Money: $' + dino.getData('money'),
+            'Fossils: ' + dino.getData('fossils'),
+            'Rank: ' + dino.getData('rank')
+        ]);
+
+        dig.setText([
+            'Event: ' + textbox.getData('event')
+        ]);
+
+        dino.on('changedata-fossils', function (gameObject, value) {
+            if (value > 10 && value <= 20)
+            {
+                gameObject.data.values.rank = "dig site assistant";
+            }
+            else if (value > 20 && value <= 30){
+                gameObject.data.values.rank = "dig site manager";
+            }
+            else if (value > 30 && value <= 40){
+                gameObject.data.values.rank = "amateur paleontologist";
+            }
+            else if (value > 40 && value <= 50){
+                gameObject.data.values.rank = "junior paleontologist";
+            }
+            else if (value > 50 && value <= 60){
+                gameObject.data.values.rank = "senior paleontologist";
+            }
+            else{
+                if (value > 60){
+                    gameObject.data.values.rank = "legendary paleontologist";  
+                } 
+            }
+
+            info.setText([
+                'Money: $' + dino.getData('money'),
+                'Fossils: ' + dino.getData('fossils'),
+                'Rank: ' + dino.getData('rank')
+            ]);
+        });
+
+        //  Change the 'value' property when the mouse is clicked
+        this.input.on('pointerdown', function () {
+            var fossilChance = Math.floor((Math.random() * 10) + 1)
+            var posChance = Math.floor((Math.random() * 9));
+            var negChance = Math.floor((Math.random() * 9));
+            // excavate.play();
+
+            if (dino.data.values.money >= 100){
+                excavate.play();
+                dino.data.values.money -= 100;
+                if (fossilChance > 5){
+                    dino.data.values.fossils += 1;
+                    textbox.data.values.event = posEvents[posChance];
+                }
+                else{
+                    textbox.data.values.event = negEvents[negChance];
+                    if (negChance == 0){
+                        dino.data.values.money -= 1000;
+                    }
+                    if (negChance == 4){
+                        dino.data.values.money -= 500;
+                    }
+                    if (negChance == 8){
+                        dino.data.values.money -= 750;
+                    }
+                }
+            }
+            else{
+                gameover.setText("GAME OVER\nReload to try again!");
+            }
+
+            info.setText([
+                'Money: $' + dino.getData('money'),
+                'Fossils: ' + dino.getData('fossils'),
+                'Rank: ' + dino.getData('rank')
+            ]);
+
+            dig.setText([
+                        textbox.getData('event')
+            ]);
+        });
+
+        this.add.image(300, 475, 'pal').setScale(.5);
+
     }
     
     update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        this.bouncy.rotation = this.physics.accelerateToObject( this.bouncy, this.input.activePointer, 500, 500, 500 );
     }
 }
 
